fb01468cd5f20c1c6d11076e6c6c71dbffed8990
sdotson <stu.dotson@gmail.com>
2016-09-19 11:42:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*
+npm version of the following curl command:
+token=`curl -s curl -H "Content-Type: application/json;charset=UTF-8" -X POST https://app.sli.do/v0.4/events/73371/auth | jq -r '.access_token'` && curl -H "Authorization: Bearer $token" -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '{"score":1}' https://app.sli.do/v0.4/events/73371/questions/763350/like
+*/
+
+const request = require('request'),
+  co = require('co'),
+  Promise = require('bluebird'),
+  requestAsync = Promise.promisify(request),
+  chalk = require('chalk'),
+  argv = require('minimist')(process.argv.slice(2)),
+  args = {
+    questionId: argv.q || '763350',
+    eventId: argv.e || '73371',
+    reps: argv.r || 1
+  };
+
+if (!args.questionId || !args.eventId) {
+  console.log(chalk.red('You must specify a question ID and an event ID'));
+} else {
+  co(function*() {
+
+    for (let i = 0; i < args.reps; i++) {
+      let authTokenResponse = yield requestAsync({
+          url: `https://app.sli.do/v0.4/events/${args.eventId}/auth`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        }),
+        authToken = JSON.parse(authTokenResponse.body).access_token,
+        likeResponse = yield requestAsync({
+          url: `https://app.sli.do/v0.4/events/${args.eventId}/questions/${args.questionId}/like`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Authorization': `Bearer ${authToken}`
+          },
+          json: {
+            "score":1
+          }
+        });
+
+      console.log(likeResponse.body);
+    }
+    console.log(chalk.cyan(`You've just up-voted question ${args.questionId} ${args.reps} times.`));
+  });
+}
